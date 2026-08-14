9354a9d60e533fa19c637322c821fe2979af566e
Nguyen Thi Hong Duyen <251020910645@st.phenikaa-uni.edu.vn>
2026-08-14 17:50:38 +0700

    Tạo các package và class

--- src/repository/MovieRepository.java
+++ src/repository/MovieRepository.java
@@ -1,4 +1,25 @@
 package repository;
 
+import model.Movie;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MovieRepository {
+    private final String file_txt = "src/data/movies.txt";
+    public ArrayList<Movie> loadMovies() {
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file_txt))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Lỗi đọc file: " + e.getMessage());
+        }
+
+        return movies;
+    }
 }
