9383929e27635139377f402bd01ebe5e756c4b11
Nguyen Thi Hong Duyen <251020910645@st.phenikaa-uni.edu.vn>
2026-08-25 13:44:34 +0700

    1

--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -1,4 +1,22 @@
 package repository;
 
+import model.Customer;
+import java.util.ArrayList;
+
 public class CustomerRepository {
+    private ArrayList<Customer> customers = new ArrayList<>();
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+    public Customer findById(String id) {
+        for (Customer customer : customers) {
+            if (customer.getId().equals(id)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Customer> getAllCustomers() {
+        return customers;
+    }
 }
--- src/repository/ShowtimeRepository.java
+++ src/repository/ShowtimeRepository.java
@@ -1,4 +1,18 @@
 package repository;
 
+import model.Showtime;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ShowtimeRepository {
+    private final String file_txt = "src/data/showtimes.txt";
+    public ArrayList<Showtime> loadShowtimes() {
+        ArrayList<Showtime> showtimes = new ArrayList<>();
+
+        return showtimes;
+    }
+
 }
