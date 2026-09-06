b7b18627eed2cf34773636defff46be7c9c93c8f
builamvu <251020912478@st.phenikaa-uni.edu.vn>
2026-09-06 12:57:18 +0700

    vu

--- src/policy/StudentPricePolicy.java
+++ src/policy/StudentPricePolicy.java
@@ -2,14 +2,13 @@ package policy;
 
 import model.Seat;
 
-public class VIPCustomerPricePolicy implements TicketPricePolicy {
+public class StudentPricePolicy implements TicketPricePolicy {
     @Override
     public double FinalTicketPrice(Seat seat, double baseFare) {
         double Sum = baseFare + seat.getSurcharge();
-        double FinalSum = Sum * 0.6;
+        double FinalSum = Sum * 0.4;
         if(FinalSum <= 0){
-            throw new IllegalArgumentException("Loi du lieu: Tong tien cuoi cung cua ban phai lon hon 0");
-        }
+            throw new IllegalArgumentException("Loi du lieu: Tong tien cuoi cung cua ban phai lon hon 0");}
         return FinalSum;
     }
 }
